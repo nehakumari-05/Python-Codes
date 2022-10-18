074ae012e19f3bca9bafc7d16b24b9f3bc794f5d
nehakumari-05 <115403937+nehakumari-05@users.noreply.github.com>
2022-10-18 17:30:27 +0530

    Add files via upload

---
+++ Print.py
@@ -0,0 +1,4 @@
+# Write a Python program to repeat the string ‘‘GOOD MORNING” n  times. Here ‘n’ is an integer entered by the user. 
+n = int(input('Enter value: ')) 
+for i in range(n):
+    print('GOOD MORNING')
