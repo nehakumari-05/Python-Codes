9e226241578d17559ba5792f00a201f94dbf468d
nehakumari-05 <115403937+nehakumari-05@users.noreply.github.com>
2022-10-18 17:29:51 +0530

    Add files via upload

---
+++ Height.py
@@ -0,0 +1,6 @@
+# Write a Python program to enter your height in cm and convert it into feet and inches. ( 12inches = 1 Foot , 2.54 cm = 1 inch) 
+ht = int(input("Enter your height in centimeters: "))
+ht_Inch = ht / 2.54;
+feet = ht_Inch // 12;
+inch = ht_Inch % 12;
+print("Your height is", feet, "feet and", inch, "inches")
