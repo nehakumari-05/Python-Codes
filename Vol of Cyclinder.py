22477f3842e9d4d1b34f92ddb2fb8c313e1d4d36
nehakumari-05 <115403937+nehakumari-05@users.noreply.github.com>
2022-10-18 17:40:45 +0530

    Add files via upload

---
+++ Vol of Cyclinder.py
@@ -0,0 +1,5 @@
+# WAP to input radius and height of a cylinder and calculate volume of cylinder. 
+radius = int(input('Enter the radius of a cylinder: '))
+height = int(input('Enter the height of a cylinder: '))
+volume = (3.14) * radius * radius * height
+print(" Volume of a cylinder: " ,volume)
