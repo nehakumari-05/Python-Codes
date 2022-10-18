cc614c3eb5c840821f83db10d40e974812c84e0e
nehakumari-05 <115403937+nehakumari-05@users.noreply.github.com>
2022-10-18 17:40:58 +0530

    Add files via upload

---
+++ Vol of Sph.py
@@ -0,0 +1,6 @@
+# The volume of a sphere with radius r is 4/3πr3 . Write a Python program to find the volume of sphere
+r= float(input('Enter the radius of a sphere: '))
+vol=( 4.0/3.0 )*(3.14)* r**3
+print('The volume of the sphere is: ',vol)
+
+
