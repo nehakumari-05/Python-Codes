e518b4690dd31c1358f8460919dd2f7826612e2c
nehakumari-05 <115403937+nehakumari-05@users.noreply.github.com>
2022-10-18 17:31:03 +0530

    Add files via upload

---
+++ Vol of Cuboid.py
@@ -0,0 +1,6 @@
+# WAP to input length ,breadth and height of a cuboid and print volume of the same. 
+l =float(input('Enter length of a cuboid: '))
+b =float(input('Enter breath of a cuboid: '))
+h =float(input('Enter height of a cuboid: '))
+vol =l*b*h
+print("Volume of Cuboid= ",vol)
