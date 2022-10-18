076af948022038ed4abd76a841149a908d037f86
nehakumari-05 <115403937+nehakumari-05@users.noreply.github.com>
2022-10-18 17:29:18 +0530

    Add files via upload

---
+++ Area of Tri.py
@@ -0,0 +1,5 @@
+# WAP to input base and height of triangle and calculate area of triangle.
+b = int(input("Input the base : "))
+h = int(input("Input the height : "))
+area = b*h/2
+print("area = ", area)
